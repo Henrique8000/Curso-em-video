d949c74be599d1846e4569e7e04559c4ffa35503
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-11-14 15:52:04 -0300

    Update ex028.py

--- ex028.py
+++ ex028.py
@@ -5,16 +5,22 @@ pelo computador. O programa deverá escrever na tela se o usuário venceu ou per
 """
 
 from random import randint
+from time import sleep
 
-numero = randint(0, 5)
 
-n_usuario = int(input("Entre 0 e 5, qual número você acha que o computador escolheu? "))
+numero = randint(0, 5)  #Faz o computador sortear um número entre 0 e 5
 
-if n_usuario == numero:
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente advinhar...')
+print('-=-' * 20)
+
+n_usuario = int(input("Entre 0 e 5, qual número você acha que o computador escolheu? "))  #Usuário entra com um número inteiro entre 0 e 5
+
+print("Processando...")
+sleep(3)
+
+if n_usuario == numero:  #Se o numero do usuário tiver o mesmo valor do numero do computador, ele irá exibir a mensagem a baixo
     print(f"Parabéns, você acertou! O número escolhido era {numero}")
 
-else:
+else:  #Se o numero do usuário for diferente, então será exibida outra mensagem
     print("Puts, você errou o número :(")
-
-
-
