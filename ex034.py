eeab22ac6310ca1e74bcc4e37a7815166b842483
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-12-23 16:47:33 -0300

    Update ex034.py
    Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
    Para salários superiores a R$1.250,00 calcule um aumento de 10%.
    Para os inferiores ou iguais, o aumento é de 15%

--- ex034.py
+++ ex034.py
@@ -4,13 +4,27 @@ Para salários superiores a R$1.250,00 calcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%
 """
 
-salario = float(input("Qual é o salário do funcionário? R$"))
-aumento = 0.00
 
-if salario <= 1250.00:
-    aumento = (salario * 15 / 100) + salario
+def main():
+    salario = float(input("Qual é o salário do funcionário? R$"))
+    lista_valores = novo_salario(salario)
+    exibe_aumento_e_salario(lista_valores)
 
-else:
-    aumento = (salario * 10 / 100) + salario
 
-print(f"Quem ganhava R${salario:.2f} passa a ganhar R${aumento:.2f} agora.")
+def novo_salario(salario):
+    aumento = 0.00
+    if salario <= 1250.00:
+        aumento = (salario * 15 / 100) + salario
+
+    else:
+        aumento = (salario * 10 / 100) + salario
+
+    lista_s_a = [salario, aumento]
+    return lista_s_a
+
+
+def exibe_aumento_e_salario(lista):
+    print(f"Quem ganhava R${lista[0]:.2f} passa a ganhar R${lista[1]:.2f} agora.")
+
+
+main()
